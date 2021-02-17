aa979c929c0d5adb24420955c6c8be076f5cfcac
Terrell Cooper <TerrellCoop@gmail.com>
2021-02-17 13:37:33 -0700

    Added ip to CIDR from leet code .

--- index.js
+++ index.js
@@ -213,3 +213,31 @@ function divisibleSumPairs(n, k, ar) {
     }
     return count;
 }
+
+
+// 751. IP to CIDR from leetCode
+var ipToCIDR = function(ip, n) {
+    const ipToLong = ip => ip.split('.')
+        .reduce((acc, x) => 256 * acc + parseInt(x), 0);
+    
+    const longToIp = long => [24, 16, 8, 0].map(i => (long >>> i) % 256).join('.');
+    const bitLength = num => Math.floor(Math.log2(num)) + 1;
+    const ans = [];
+    
+    let long = ipToLong(ip);
+    
+    while (n) {
+        let mask = Math.max(
+            33 - bitLength(long & -long),
+            33 - bitLength(n)
+        );
+        
+        ans.push(longToIp(long) + '/' + mask);
+        long += 1 << (32 - mask);
+        n -= 1 << (32 - mask);
+    }
+    
+    let temp = ans.map(a => a = a === '0.0.0.0/Infinity' ? '0.0.0.0/32' : a);
+
+    return temp;
+};
