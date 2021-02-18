f370e99abbc67bff9d681b31dc44df927a0a4b01
Terrell Cooper <TerrellCoop@gmail.com>
2021-02-18 15:15:43 -0700

    Added apples and oranges from leetcode

--- index.js
+++ index.js
@@ -241,3 +241,21 @@ var ipToCIDR = function(ip, n) {
 
     return temp;
 };
+
+// Apples and oranges from hacker ranck 
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let totalApples = 0;
+    let totalOranges = 0;
+    for (let i = 0; i <= apples.length; i++) {
+        if (a + apples[i] >= s && a + apples[i] <= t) {
+            totalApples += 1;
+        }
+    }
+    for (let i = 0; i <= oranges.length; i++) {
+        if (b + oranges[i] >= s && b + oranges[i] <= t) {
+            totalOranges += 1;
+        }
+    }
+    console.log(totalApples);
+    console.log(totalOranges);
+}
